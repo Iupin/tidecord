c74db5c4135377b33e3c38684bc6cf6e3aec2b1f
Matthew <matthewduff0912@gmail.com>
2026-03-03 16:53:17 +0000

    Init

---
+++ MediaBridge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Windows.Media.Control;
+
+class Program
+{
+    static async Task Main()
+    {
+        while (true)
+        {
+            try
+            {
+                var manager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+                var sessions = manager.GetSessions();
+
+                foreach (var session in sessions)
+                {
+                    if (!session.SourceAppUserModelId.ToLower().Contains("tidal"))
+                        continue;
+
+                    var info = await session.TryGetMediaPropertiesAsync();
+                    var timeline = session.GetTimelineProperties();
+                    var playback = session.GetPlaybackInfo();
+
+                    var output = new
+                    {
+                        title = info.Title,
+                        artist = info.Artist,
+                        duration = timeline.EndTime.TotalMilliseconds,
+                        position = timeline.Position.TotalMilliseconds,
+                        paused = playback.PlaybackStatus != GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing
+                    };
+
+                    Console.WriteLine(JsonSerializer.Serialize(output));
+                    break;
+                }
+            }
+            catch { }
+
+            await Task.Delay(2000);
+        }
+    }
+}
