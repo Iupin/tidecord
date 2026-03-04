723502aa4788d20306f878b4c6268ea621229898
Matthew <matthewduff0912@gmail.com>
2026-03-04 17:23:53 +0000

    feat: tray menu includes status

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { app, Tray, Menu } from "electron";
 import path from "path";
 import { startBridge, getCurrentSong, stopBridge } from "./media";
 import { initRPC, updatePresence, clearPresence } from "./rpc";
+import { Song } from "./media";
 
 let tray: Tray | null = null;
 
@@ -16,20 +17,25 @@ app.whenReady().then(async () => {
 
   tray = new Tray(iconPath)
 
-  const contextMenu = Menu.buildFromTemplate([
-    { label: "Tidecord Running", enabled: false },
-    { type: "separator" },
-    {
-      label: "Quit",
-      click: () => {
-        stopBridge();
-        app.quit();
-      }
-    }
-  ]);
+  function updateTray(song: Song) {
+    const statusLabel = song.title == "TIDAL"
+      ? `Idle`
+      : `Playing: ${song.title}`
 
-  tray.setToolTip("Tidecord");
-  tray.setContextMenu(contextMenu);
+    const contextMenu = Menu.buildFromTemplate([
+      { label: `Tidecord - ${statusLabel}`, enabled: false },
+      { type: "separator" },
+      {
+        label: "Quit",
+          click: () => {
+          stopBridge();
+          app.quit();
+        }
+      }
+    ]);
+    tray?.setToolTip("Tidecord");
+    tray?.setContextMenu(contextMenu);
+  }
 
   app.setLoginItemSettings({ openAtLogin: true });
 
@@ -40,6 +46,7 @@ app.whenReady().then(async () => {
     const song = getCurrentSong();
     if (!song) return clearPresence();
     updatePresence(song);
+    updateTray(song)
   }, 5000);
 });
 
